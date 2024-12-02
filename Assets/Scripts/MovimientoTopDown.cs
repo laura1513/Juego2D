74f33ac580fb4a87a32941d678ab1a434b2f2372
Aaronfc19 <aaronfc2000@gmail.com>
2024-12-02 19:51:19 +0100

    Melee, cambios en movimiento top down,, meele detecta tag enemy

--- Assets/Scripts/MovimientoTopDown.cs
+++ Assets/Scripts/MovimientoTopDown.cs
@@ -12,6 +12,13 @@ public class MovimientoTopDown : MonoBehaviour
     [SerializeField] private Transform posFlecha;
     private Vector2 pointerInput, movementInput;
 
+    //Ataque
+    [SerializeField] private Transform attackPont;
+    [SerializeField] private float attackRadius;
+    //[SerializeField] private GameObject particulas;
+    [SerializeField] private float foerzaAtaque;
+    //Ataque
+
     private PlayerInput _playerinput;
     private Rigidbody2D _rb;
     private Vector2 _pos;
@@ -35,13 +42,15 @@ public class MovimientoTopDown : MonoBehaviour
 
     private void Update()
     {
-        //pointerInput = GetPointerInput();
-        
-        _pos = _playerinput.actions["Mover"].ReadValue<Vector2>();
+        if (!_atacando)
+        {
+            //pointerInput = GetPointerInput();
+
+            _pos = _playerinput.actions["Mover"].ReadValue<Vector2>();
 
         //Para mover el personaje
         _transform.position += new Vector3(_pos.x * moveVel * Time.deltaTime, _pos.y * moveVel * Time.deltaTime, 0);
-
+        }
         //Para rotar el personaje
         //_animator.SetFloat("velX", Mathf.Abs(_pos.x));
         //_animator.SetFloat("velY", Mathf.Abs(_pos.y));
@@ -56,22 +65,7 @@ public class MovimientoTopDown : MonoBehaviour
             _animator.SetBool("1_Move", false);
         }
     }
-    public void Ataque(InputAction.CallbackContext context)
-    {
-        
-        if (context.started && !_atacando)
-        {
-            _rb.velocity = Vector2.zero;
-            _animator.SetTrigger("Ataque");
-        }
-    }
-    
-    public void Atacando() {
-        _atacando = true;
-    }
-    public void NoAtacando() {
-        _atacando = false;
-    }
+ 
     public void Flecha(InputAction.CallbackContext context)
     {
        
@@ -101,19 +95,50 @@ public class MovimientoTopDown : MonoBehaviour
         {
             _rb.velocity = Vector2.zero;
             //_animator.SetBool("Disparar", true);
-            Atacando();
+            _atacando = true;
         }
     }
     public void Disparando() { _atacando = true; _animator.SetBool("Disparar", true); }
     public void NoDisparando() { _atacando = false; _animator.SetBool("Disparar", false); }
-    // Video camara aim https://youtu.be/DPqc7qYDtzM?si=ZONzmozuQsopG8gZ
+    // todo aqui es de Ataque
+    public void Melee(InputAction.CallbackContext context)
+    {
+        if (context.started && !_atacando)
+        {
+            _rb.velocity = Vector2.zero;
+            Golpe();
+        }
+    }
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(attackPont.position, attackRadius);
+    }
+    public void Golpe() { _atacando = true; _animator.SetBool("Melee", true); }
+    public void NoGolpe() { _atacando = false; _animator.SetBool("Melee", false); }
 
-    //private Vector2 GetPointerInput()
-    // {
-    //Vector3 mousePos = _playerinput.actions["PointerPosition"].ReadValue<Vector2>();
-    // mousePos.z = Camera.main.nearClipPlane;
-    // Debug.Log(mousePos);
-    // return Camera.main.ScreenToWorldPoint(mousePos);
+    public void EventoAnimAtaque()
+    {
+        //Necesario para el circulo
+        ContactFilter2D aux = new ContactFilter2D();
+        List<RaycastHit2D> results = new List<RaycastHit2D>();
 
-    // }
+        // se crea el circulo
+        Physics2D.CircleCast(attackPont.position, attackRadius, Vector2.zero, aux, results);
+
+        //Lee la lista
+        foreach (RaycastHit2D hit in results)
+        {
+            if (hit.collider.gameObject.CompareTag("Enemy"))
+            {
+                Debug.Log("Golpeado");
+                //Instantiate(particulasAtaque, hit.collider.transform.position, hit.collider.transform.rotation); genera particulas
+                // hit.collider.gameObject.GetComponent<IA_Enemigo>().Golpear(); tiene el quitar vida en el enemigo
+            }
+            else
+            {
+                //Instantiate(particulasAtaque, attackPoint.position, attackPoint.rotation); Genera particulas si golpea otra cosa
+            }
+        }
+    }
 }
